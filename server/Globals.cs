e68161cd78b6634bfa7dc00cac6ad0deadf80874
DJ Aaron <koreangeekman@gmail.com>
2023-11-29 13:14:17 -0700

    dotnet-vue template init

---
+++ server/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using wk10.Repositories;
+global using wk10.Services;
+global using wk10.Models;
