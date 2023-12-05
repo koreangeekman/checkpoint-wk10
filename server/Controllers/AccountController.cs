546812094b03abff97f463b203db3daa23edf28f
DJ Aaron <koreangeekman@gmail.com>
2023-12-05 09:13:42 -0700

    refactor back from primary constructor

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -2,20 +2,28 @@ namespace wk10.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class AccountController(
-  AccountService accountService,
-  Auth0Provider auth0Provider,
-  FavService favService
-  ) : ControllerBase
+public class AccountController : ControllerBase
 {
+
+  private readonly Auth0Provider _a0;
+  private readonly AccountService _accountService;
+  private readonly FavService _favService;
+
+  public AccountController(RecipeService recipeService, AccountService accountService, Auth0Provider a0, FavService favService)
+  {
+    _a0 = a0;
+    _accountService = accountService;
+    _favService = favService;
+  }
+
   [HttpGet]
   [Authorize]
   public async Task<ActionResult<Account>> Get()
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-      return Ok(accountService.GetOrCreateProfile(userInfo));
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_accountService.GetOrCreateProfile(userInfo));
     }
     catch (Exception e)
     {
@@ -29,8 +37,8 @@ public class AccountController(
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-      return Ok(favService.GetFavoritesByAccountId(userInfo.Id));
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_favService.GetFavoritesByAccountId(userInfo.Id));
     }
     catch (Exception e) { return BadRequest(e.Message); }
   }
@@ -41,8 +49,8 @@ public class AccountController(
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-      return Ok(accountService.Edit(editData, userInfo.Email));
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_accountService.Edit(editData, userInfo.Email));
     }
     catch (Exception e) { return BadRequest(e.Message); }
   }
--- server/Controllers/RecipesController.cs
+++ server/Controllers/RecipesController.cs
@@ -2,30 +2,38 @@ namespace wk10.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class RecipesController(
-    RecipeService recipeService,
-    IngredientsService ingredientsService,
-    Auth0Provider auth0Provider
-  ) : ControllerBase
+public class RecipesController : ControllerBase
 {
+  private readonly RecipeService _recipeService;
+  private readonly IngredientsService _ingredientsService;
+  private readonly Auth0Provider _a0;
+
+  public RecipesController(Auth0Provider a0, RecipeService recipeService, IngredientsService ingredientsService)
+  {
+    _a0 = a0;
+    _recipeService = recipeService;
+    _ingredientsService = ingredientsService;
+  }
+
+
   [HttpGet]
   public ActionResult<List<Recipe>> GetRecipes()
   {
-    try { return Ok(recipeService.GetRecipes()); }
+    try { return Ok(_recipeService.GetRecipes()); }
     catch (Exception e) { return BadRequest(e.Message); }
   }
 
   [HttpGet("{recipeId}")]
   public ActionResult<Recipe> GetRecipeById(int recipeId)
   {
-    try { return Ok(recipeService.GetRecipeById(recipeId)); }
+    try { return Ok(_recipeService.GetRecipeById(recipeId)); }
     catch (Exception e) { return BadRequest(e.Message); }
   }
 
   [HttpGet("{recipeId}/ingredients")]
   public ActionResult<List<Ingredient>> GetIngredientsByRecipeId(int recipeId)
   {
-    try { return Ok(ingredientsService.GetIngredientsByRecipeId(recipeId)); }
+    try { return Ok(_ingredientsService.GetIngredientsByRecipeId(recipeId)); }
     catch (Exception e) { return BadRequest(e.Message); }
   }
 
@@ -35,9 +43,9 @@ public class RecipesController(
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
       recipeData.CreatorId = userInfo.Id;
-      return Ok(recipeService.CreateRecipe(recipeData));
+      return Ok(_recipeService.CreateRecipe(recipeData));
     }
     catch (Exception e) { return BadRequest(e.Message); }
   }
@@ -48,8 +56,8 @@ public class RecipesController(
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-      return Ok(recipeService.DeleteRecipe(userInfo.Id, recipeId));
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_recipeService.DeleteRecipe(userInfo.Id, recipeId));
     }
     catch (Exception e) { return BadRequest(e.Message); }
   }
@@ -60,8 +68,8 @@ public class RecipesController(
   {
     try
     {
-      Account userInfo = await auth0Provider.GetUserInfoAsync<Account>(HttpContext);
-      return Ok(recipeService.UpdateRecipe(recipeId, recipeData));
+      Account userInfo = await _a0.GetUserInfoAsync<Account>(HttpContext);
+      return Ok(_recipeService.UpdateRecipe(recipeId, recipeData));
     }
     catch (Exception e) { return BadRequest(e.Message); }
   }
